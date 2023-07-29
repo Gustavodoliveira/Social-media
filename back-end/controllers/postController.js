fe434dab228ac0bd163233f84b1912c1bed3d6a7
Gustavo <gustavod.oliveira@outlook.com>
2023-07-28 21:01:31 -0300

    meet: start post route

--- back-end/controllers/postController.js
+++ back-end/controllers/postController.js
@@ -1,7 +1,8 @@
+import getToken from '../middleware/get-Token';
 import Posts from '../models/Post';
 
 module.exports = class postController {
   static async Postar(req, res) {
-
+    const token = getToken();
   }
 };
