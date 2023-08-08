2241c38f32c4538bf2ece6d663d7e525d3f37eeb
Gustavo <gustavod.oliveira@outlook.com>
2023-08-07 21:40:11 -0300

    Meet : init redux

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import Store from './store';
 import Register from './pages/Register/Register';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Register />} />
-      </Routes>
-    </Router>
+    <Provider store={Store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ front-end/src/store/index.js
@@ -0,0 +1,5 @@
+import { legacy_createStore as createStore } from 'redux';
+
+const Store = createStore();
+
+export default Store;
