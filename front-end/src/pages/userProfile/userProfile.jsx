7789c1a90d73b0c1a63b357f2e3e2078b89e5e9e
Gustavo <gustavod.oliveira@outlook.com>
2023-08-28 21:31:07 -0300

    Meet: init conection with route api edit user

--- front-end/src/pages/userProfile/userProfile.jsx
+++ front-end/src/pages/userProfile/userProfile.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
 import api from '../../services/api';
 
 import Header from '../../components/header/header';
+import Input from '../../components/input/input';
+import { Container } from '../Register/styleRegister';
 
 function UserProfile() {
   const [user, setUser] = useState({});
@@ -16,10 +20,74 @@ function UserProfile() {
       setUser(resp.data.user);
     });
   }, [token]);
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+  async function handleClick(e) {
+    e.preventDefault();
+    await api.patch(`/user/edit/${user._id}`, (user), {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+      },
+    })
+      .then((resp) => toast.success(resp.data.message))
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <>
       <Header />
       <div>userProfile</div>
+      <Container>
+        <ToastContainer autoClose={3000} />
+        <h1>Create your account</h1>
+        <p>create your account and have fun with your friends</p>
+        <form onSubmit={handleSubmit}>
+          <Input
+            type="text"
+            name="name"
+            placeHolder="type your name"
+            handleOnChange={handleChange}
+            value={user.name}
+
+          />
+          <Input
+            type="text"
+            name="phone"
+            placeHolder="type your phone"
+            handleOnChange={handleChange}
+            value={user.phone}
+          />
+          <Input
+            type="email"
+            name="email"
+            placeHolder="type your e-mail"
+            handleOnChange={handleChange}
+            value={user.email}
+          />
+          <Input
+            type="password"
+            name="password"
+            placeHolder="type your password"
+            handleOnChange={handleChange}
+          />
+          <Input
+            type="password"
+            name="confirmpassword"
+            placeHolder="confirm your password"
+            handleOnChange={handleChange}
+          />
+          <input type="submit" value="Register" onClick={handleClick} />
+
+        </form>
+      </Container>
     </>
   );
 }
